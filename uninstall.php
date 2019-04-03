d64153b2b394d83bd9fd8cdf697784be9d8a1d9e
Chris Nelson <cnelson@printaura.com>
2019-04-03 16:21:03 -0400

    beautify
    formatting changes only

--- uninstall.php
+++ uninstall.php
@@ -1,24 +1,26 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-define( 'REDE_PLUGIN_BASE_PATH', plugin_dir_path(__FILE__) );
-require_once( plugin_dir_path(__FILE__) . 'classes/class-rede-helpers.php' );
-require_once( plugin_dir_path(__FILE__) . 'printaura-woocommerce-api-core.php' );
-function printaura_woocommerce_json_api_uninstall() {
-  global $wpdb;
-  if ( !defined('WP_UNINSTALL_PLUGIN') ) {
+if (! defined('ABSPATH')) {
     exit;
-  }
-  $helpers = new JSONAPIHelpers();
-
-  $json_api_slug = get_option( $helpers->getPluginPrefix() . '_slug' );
-  if ( ! $json_api_slug ) {
-    // Ooops, no api slug?
-  } else {
-    $found = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM " . $wpdb->posts . " WHERE post_name = %s LIMIT 1;", $page['post_name'] ) );
-    if ( $found ) {
-      wp_delete_post($found,true);
+} // Exit if accessed directly
+define('REDE_PLUGIN_BASE_PATH', plugin_dir_path(__FILE__));
+require_once(plugin_dir_path(__FILE__) . 'classes/class-rede-helpers.php');
+require_once(plugin_dir_path(__FILE__) . 'printaura-woocommerce-api-core.php');
+function printaura_woocommerce_json_api_uninstall()
+{
+    global $wpdb;
+    if (!defined('WP_UNINSTALL_PLUGIN')) {
+        exit;
     }
-  }
+    $helpers = new JSONAPIHelpers();
 
+    $json_api_slug = get_option($helpers->getPluginPrefix() . '_slug');
+    if (! $json_api_slug) {
+        // Ooops, no api slug?
+    } else {
+        $found = $wpdb->get_var($wpdb->prepare("SELECT ID FROM " . $wpdb->posts . " WHERE post_name = %s LIMIT 1;", $page['post_name']));
+        if ($found) {
+            wp_delete_post($found, true);
+        }
+    }
 } // end woocommerce_json_api_activate
 printaura_woocommerce_json_api_uninstall();
